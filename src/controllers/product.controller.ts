b3761d33537ddd9920adda519fa96208bf68e785
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-02-18 12:30:21 +0100

    feat(product) : complete CRUD for Product API

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
 import { ProductService } from '../services/product.service';
 
 @Controller('product')
@@ -26,7 +26,24 @@ export class ProductController {
     }
 
     @Get(':id')
-    getProduct(@Param('id') productId: string) {
+    getProduct(
+        @Param('id') productId: string
+    ) {
         return this.productService._getById(productId);
+    }
+
+    @Patch(':id')
+    editProduct(
+        @Param('id') productId: string, 
+        @Body('title') title: string,
+        @Body('description') description: string,
+        @Body('price') price: number,
+    ) {
+        return this.productService._editProduct(productId, title, description, price);
+    }
+
+    @Delete(':id')
+    deleteProduct(@Param('id') productId) {
+        return this.productService._deleteProduct(productId);
     }
 }
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -17,12 +17,59 @@ export class ProductService {
     }
 
     _getById(id: string) {
-        const product = this._products.find(p => p.id === id);
+        const product = this.findOneProduct(id);
         if (!product) {
             throw new NotFoundException('Product Not Found')
         }
 
         return {...product}
+
+    }
+
+    private findOneProduct(id: string) {
+        const product = this._products.find(p => p.id === id);
+        return {...product}
+    }
+
+    private findProduct(id: string) : [Product, number] {
+        const productIndex = this._products.findIndex(p => p.id === id);
+        const product = this._products[productIndex];
+               return [product, productIndex];
+    }
+
+    _editProduct(id: string, title: string, description: string, price: number) {
+        const [product, index] = this.findProduct(id);
+        if (!product) {
+            throw new NotFoundException('Product Not Found')
+            
+        }
+        const upadatedProduct = {...product};
+
+        if (title) upadatedProduct.title = title;
+        if (description) upadatedProduct.description = description;
+        if (price) upadatedProduct.price = price;
+
+        this._products[index] = upadatedProduct;
+        return {...this._products[index]}
+
+    }
+
+    _deleteProduct(id: string) {
+        const index = this.findProduct(id)[1];
+        
+        if (index != -1) {
+            this._products.splice(index, 1);
+            return {
+                success: true,
+                message: "Product removed Successfully!"
+            }
+
+        } else {
+            throw new NotFoundException('Product Not Found');
+            
+        }
+            
+       
 
     }
 }
